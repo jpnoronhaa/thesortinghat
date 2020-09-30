c1158b4d7caa6bf8bd82e46046fa26ef3ad23755
João Pedro Noronha <joaopdrons@gmail.com>
2020-09-30 16:14:52 -0300

    Sorting Page Styled

--- src/pages/Sorting/index.tsx
+++ src/pages/Sorting/index.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
-import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { Container, Background, Content } from './styles';
+
+import title from '../../assets/title.png';
+import ravenclaw from '../../assets/ravenclaw.jpg';
+import hufflepuff from '../../assets/hufflepuff.jpg';
+import gryffindor from '../../assets/gryffindor.jpg';
+import slytherin from '../../assets/slytherin.jpg';
 
 const Sorting: React.FC = () => {
-  return <h1>The Sorting Hat Cerimony</h1>;
+  return (
+    <Container>
+      <Content>
+        <img src={title} alt="The Sorting Hat" />
+        <button type="button">What house your going to be?</button>
+        <img src={ravenclaw} alt="Ravenclaw" />
+        <button type="button">See more about your house!</button>
+        <a href="https://github.com/japanoronha">
+          See more on my GitHub profile
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
 };
 
 export default Sorting;
--- src/pages/Sorting/styles.ts
+++ src/pages/Sorting/styles.ts
@@ -1,7 +1,50 @@
 import styled from 'styled-components';
+import background from '../../assets/background.jpg';
 
 export const Container = styled.div`
-  * {
-    margin: 0;
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  place-content: center;
+  align-items: center;
+  margin-top: px;
+
+  width: 100%;
+  max-width: 700px;
+
+  button + img {
+    height: 500px;
+    width: 341.064120055px;
+    align-self: center;
+
+    margin-top: 20px;
+    margin-bottom: 20px;
   }
+
+  button {
+    background: #ff9000;
+    border-radius: 10px;
+    border: 0;
+    padding: 0 16px;
+    width: 340px;
+    height: 56px;
+    color: #312e38;
+    font-weight: 500;
+    cursor: pointer;
+  }
+
+  a {
+    margin-top: 20px;
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${background}) center;
 `;
