ef757f133fe695efa6e5841bb829b7ee670a8e88
João Pedro Noronha <joaopdrons@gmail.com>
2020-10-01 14:02:51 -0300

    Connected with the API and The Sorting Ceremony prepared

--- src/pages/Sorting/index.tsx
+++ src/pages/Sorting/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import { Container, Background, Content } from './styles';
 
 import title from '../../assets/title.png';
@@ -8,12 +9,40 @@ import gryffindor from '../../assets/gryffindor.jpg';
 import slytherin from '../../assets/slytherin.jpg';
 
 const Sorting: React.FC = () => {
+  const [house, setHouse] = useState<string>('Ravenclaw');
+
+  function selectHouse(): any {
+    let img = ravenclaw;
+    if (house === 'Ravenclaw') {
+      img = ravenclaw;
+    } else if (house === 'Hufflepuff') {
+      img = hufflepuff;
+    } else if (house === 'Gryffindor') {
+      img = gryffindor;
+    } else {
+      img = slytherin;
+    }
+
+    return img;
+  }
+
   return (
     <Container>
       <Content>
         <img src={title} alt="The Sorting Hat" />
-        <button type="button">What house your going to be?</button>
-        <img src={ravenclaw} alt="Ravenclaw" />
+        <button
+          type="button"
+          onClick={() => {
+            axios
+              .get<string>('https://www.potterapi.com/v1/sortingHat')
+              .then(response => {
+                setHouse(response.data);
+              });
+          }}
+        >
+          What house your going to be?
+        </button>
+        <img src={selectHouse()} alt={house} />
         <button type="button">See more about your house!</button>
         <a href="https://github.com/japanoronha">
           See more on my GitHub profile
